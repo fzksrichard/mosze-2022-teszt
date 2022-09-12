3567989f6029862e0fbaa6fcc9ed6627a1e512f2
fzksrichard <113343759+fzksrichard@users.noreply.github.com>
2022-09-12 22:31:24 +0200

    Update main.cpp
    Hibák jelezve

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //hibás változónév (N_ELEMENTS)
+    std::cout << '1-100 ertekek duplazasa' //aposztróf használva idézőjel helyett, pontosvessző hiányzik sor végén
+    for (int i = 0;) //hiányzik a ciklusfeltétel és a léptetés üteme
     {
-        b[i] = i * 2;
+        b[i] = i * 2; //(i+1). elemnek kell a kétszeresét venni
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //hibás ciklusfeltétel
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //hiányzik az értékkiíratás és a pontosvessző
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; //értékadás (0) hiányzik
+    for (int i = 0; i < N_ELEMENTS, i++) //szintaktikai hiba
     {
-        atlag += b[i]
+        atlag += b[i] //pontosvessző hiányzik
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
